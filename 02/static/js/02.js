8c89eee2d89e6765366d865f193b0bd365be051b
ghdaud30 <54072339+ghdaud30@users.noreply.github.com>
2023-08-22 15:04:58 +0900

    08 22 수업 내용
    Html 수업 내용 업로드

---
+++ 02/static/js/02.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function (){
+    document.getElementById("ah1").innerHTML = "JAVASCRIPT"
+})
+
+function hello(){
+    alert("Hello Everyone");
+    console.log("Hello My Friends")
+}
